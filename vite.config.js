cd118eabcd2ca9a379b346ca16731fbbf75753fd
twodownmike <mcross162@gmail.com>
2025-11-20 06:54:27 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/lifesync/', // IMPORTANT: This must match your repository name
+  base: '/LifeSync/', // IMPORTANT: This must match your repository name
 })
